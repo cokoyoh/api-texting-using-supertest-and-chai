11f9ce4a993aaff2338c0731100c5e0f26c44e1b
cokoyoh <charlesokoyoh@gmail.com>
2020-12-08 16:10:09 +0300

    Set up mocky.io response test

--- tests/first.spec.js
+++ tests/first.spec.js
@@ -18,4 +18,10 @@ describe('First test', () => {
         expect(res.statusCode).to.be.equal(200);
       })
   })
+
+  it('mocky ok response', (done) => {
+    request('https://run.mocky.io')
+      .get('/v3/962076b4-5ccf-44c4-ad13-1c1ed1070c78')
+      .expect(200, done)
+  })
 })
